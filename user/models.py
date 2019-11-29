1b9aa72bf446307819fc8e59e927c151f42690f5
germang2 <germandg99@gmail.com>
2019-11-29 00:54:40 -0500

    App User and Model CustomModel, using default User as a ForeignKey

---
+++ user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class CustomUser(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.user.email
+    
