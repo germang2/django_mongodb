abf9e96fc8287451306b554469c88f9ee9819da7
germang2 <germandg99@gmail.com>
2019-11-30 00:01:17 -0500

    Method for creation of users and validations

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import CustomUser as User
+from django.contrib.auth.models import User
+from .models import CustomUser
 
 class UserSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=100)
@@ -7,7 +8,33 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
 
 class CustomUserSerializer(serializers.Serializer):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ]
     user = UserSerializer()
-    age = serializers.IntegerField()
-    gender = serializers.CharField(max_length=20)
+    age = serializers.IntegerField(min_value=0, max_value=120)
+    gender = serializers.ChoiceField(GENDER_CHOICES)
     created_at = serializers.DateTimeField(read_only=True)
+
+    """ validate that the user is unique, validating email """
+    def validate_user(self, value):
+        email = value['email']
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('This email already exists')
+        return value
+
+ 
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['user']['email'],
+            email=validated_data['user']['email'],
+            first_name=validated_data['user']['first_name'],
+            last_name=validated_data['user']['last_name']
+        )
+        return CustomUser.objects.create(
+            user=user, 
+            age=validated_data['age'],
+            gender=validated_data['gender']
+        )
--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
