c716bfc9ad0d016ff68c85d803956979389f0f3e
germang2 <germandg99@gmail.com>
2019-12-01 11:03:31 -0500

    Handling errors with filters for get users

--- user/views.py
+++ user/views.py
@@ -12,18 +12,22 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
 
     def list(self, request, *args, **kwargs):
-        filters = request.query_params['query']
+        params = request.query_params
+        filters = params['query'] if 'query' in params else None
         queryset = CustomUser.objects.all()
         if filters is not None:
-            json_query = json.loads(filters)
-            if 'name' in json_query:
-                queryset = queryset.filter(user__first_name__icontains=json_query['name'])
-            if 'last_name' in json_query:
-                queryset = queryset.filter(user__last_name__icontains=json_query['last_name'])
-            if 'gender' in json_query:
-                queryset = queryset.filter(gender=json_query['gender'].lower())
-            if 'email' in json_query:
-                queryset = queryset.filter(user__email__icontains=json_query['email'])
+            try:
+                json_query = json.loads(filters)
+                if 'name' in json_query:
+                    queryset = queryset.filter(user__first_name__icontains=json_query['name'])
+                if 'last_name' in json_query:
+                    queryset = queryset.filter(user__last_name__icontains=json_query['last_name'])
+                if 'gender' in json_query:
+                    queryset = queryset.filter(gender=json_query['gender'].lower())
+                if 'email' in json_query:
+                    queryset = queryset.filter(user__email__icontains=json_query['email'])
+            except:
+                pass
 
         page = self.paginate_queryset(queryset)
         serializer = CustomUserSerializer(queryset, many=True)
