2322f0e162ccdb6ced9877aa0cf49fc1cb74d6da
germang2 <germandg99@gmail.com>
2019-12-01 10:11:37 -0500

    Filter to get users, can search using name, last_name, email and gender all at same time and using pagination

--- user/views.py
+++ user/views.py
@@ -4,8 +4,28 @@ from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 from django.contrib.auth.models import User
+import json
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        filters = request.query_params['query']
+        queryset = CustomUser.objects.all()
+        if filters is not None:
+            json_query = json.loads(filters)
+            if 'name' in json_query:
+                queryset = queryset.filter(user__first_name__icontains=json_query['name'])
+            if 'last_name' in json_query:
+                queryset = queryset.filter(user__last_name__icontains=json_query['last_name'])
+            if 'gender' in json_query:
+                queryset = queryset.filter(gender=json_query['gender'].lower())
+            if 'email' in json_query:
+                queryset = queryset.filter(user__email__icontains=json_query['email'])
+
+        page = self.paginate_queryset(queryset)
+        serializer = CustomUserSerializer(queryset, many=True)
+            
+        return self.get_paginated_response(serializer.data)
